ce56604aa08fcfc3fe4713c827e993e2f6ecebe2
SeanQuinn <Seanquinn917@gmail.com>
2022-12-26 14:47:56 -0500

    commit tree abfb0e8b7bbac0fda434b8b33a13a78851707b08

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+let value;
+function distanceFromHqInBlocks(value){
+    if (value >= 42)
+    return value - 42;
+    else (value <= 42)
+    return 42 - value;
+};
+
+function distanceFromHqInFeet(value){
+    return distanceFromHqInBlocks(value) *264;
+};
+
+function distanceTravelledInFeet(start, destination){
+    if (start <= destination)
+    return (destination - start) *264;
+    else return (start - destination) * 264
+}
+    function calculatesFarePrice(start, destination){
+       let distance = distanceTravelledInFeet(start, destination);
+        if (distance <= 400){
+        return 0;
+        }
+        else if (distance >= 400 && distance <= 2000){
+        return (distance - 400) * 0.02;
+        }
+        else if (distance >= 2000 && distance <=2499){
+            return 25;
+        }
+        else if (distance > 2500){
+        return "cannot travel that far";
+        }
+    };
+
